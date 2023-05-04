a7b85036b7220c364c32df3687ddbcea7017d638
lkzs2003 <loczeq2203@gmail.com>
2023-05-04 17:21:58 +0200

    fifth commit

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """Inicjalizacja statku kosmicznego i jego położenie początkowe"""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Wczytywanie statku kosmicznego i pobranie jego prostokąta
@@ -16,6 +17,21 @@ class Ship:
 
         # Każdy nowy statek pojawia się na dole ekranu
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Położenie poziomu staktu (typ float)
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Uaktualnienie obiectu rect na podstawie wartości self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Wyświetlanie statku w jego akutalnym położeniu"""
