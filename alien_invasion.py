ffe368c58de171893626166364f3c52deb717b20
lkzs2003 <loczeq2203@gmail.com>
2023-05-05 22:57:21 +0200

    twelfth commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -66,10 +66,22 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
+
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("The ship was hit!!!")
 
     def _create_fleet(self):
         alien = Alien(self)
@@ -102,7 +114,7 @@ class AlienInvasion:
 
     def _change_fleet_direction(self):
         for alien in self.aliens.sprites():
-            alien.rect.y += self.settings.fleer_drop_speed
+            alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
 
 
@@ -119,7 +131,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
 
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
--- settings.py
+++ settings.py
@@ -6,10 +6,10 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
-        self.alien_speed = 1.0
-        self.fleer_drop_speed = 10
+        self.alien_speed = 0.25
+        self.fleet_drop_speed = 10
         self.fleet_direction = 1
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.25
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
